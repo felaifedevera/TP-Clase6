95076dd3bfb888a6cb952454fd603fdd0785a759
felaifedevera <132582762+felaifedevera@users.noreply.github.com>
2024-04-15 17:15:07 -0300

    nav en js

---
+++ index.js
@@ -0,0 +1,29 @@
+const crearLista = (elementos) => {
+    const ul = document.createElement('ul');
+    ul.classList.add('nav-bar');
+  
+    elementos.forEach(elemento => {
+      const li = document.createElement('li');
+      li.classList.add('tituloNav');
+  
+      const a = document.createElement('a');
+      a.href = elemento.href;
+      a.textContent = elemento.texto;
+  
+      li.appendChild(a);
+      ul.appendChild(li);
+    });
+  
+    return ul;
+  };
+  
+  const elementosNav = [
+    { href: '#', texto: 'Acerca de' },
+    { href: '#', texto: 'Servicios' },
+    { href: '#', texto: 'Servicios' },
+    { href: '#', texto: 'Informacion' },
+    { href: '#', texto: 'biografia' }
+  ];
+  
+  const nav = crearLista(elementosNav);
+  document.querySelector('.contenedor').appendChild(nav);
