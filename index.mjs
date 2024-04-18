21623995081149538204c214d546112e53fa09b3
BrandonAlanDev <brandoncarabajal@gmail.com>
2024-04-18 11:07:48 -0300

    texto card

--- index.mjs
+++ index.mjs
@@ -97,9 +97,9 @@ const crearLista = (elementos) => {
                     console.log('holaa');
     
                   const cards =[
-                    { href: '#',id:'p1',imagenURL :'IMG/argentina.jpg',texto: 'Tecnologia', tituloError : 'No cargo la imagen' },
-                    { href: '#',id:'p2',imagenURL :'IMG/francia.jpg', texto: 'Deportes', tituloError : 'No cargo la imagen' },
-                    { href: '#',id:'p3',imagenURL :'IMG/belgica.webp',texto: 'Noticias', tituloError : 'No cargo la imagen' },
+                    { href: '#',id:'p1',imagenURL :'IMG/argentina.jpg',texto: 'Argentina lidera el ranking fifa por su desempeño en los ultimos años siendo campeon del mundial en qatar 2022, campon de america y campeon de la finalizima ante italia teniendo un total de 1858 pts"', tituloError : 'No cargo la imagen' },
+                    { href: '#',id:'p2',imagenURL :'IMG/francia.jpg', texto: 'Francia es la segunda seleccion mejor rankeada en el ranking fifa dado a el subcampeonato mundial 2022, campeon 2018 teniendo 1840.59 pts', tituloError : 'No cargo la imagen' },
+                    { href: '#',id:'p3',imagenURL :'IMG/belgica.webp',texto: 'Belgica se queda con el tercer puesto dado que siempre clasifica a los grandes torneos teniendo un invicto de cinco años en su estadio 8 victorias y cinco empates teniendo asi 1795.23 pts en ranking fifa', tituloError : 'No cargo la imagen' },
                   ];
                 
                   const kard  = CrearCard(cards);
