b93da9db839632c0e1cc38e877efb4ce5eea1aeb
felaifedevera <132582762+felaifedevera@users.noreply.github.com>
2024-04-19 19:42:11 -0300

    areglado problema de duplicados

--- index.mjs
+++ index.mjs
@@ -1,5 +1,6 @@
 let eventoEjecutado = false;
 var seleccion;
+let eventocard = false ;
 
 document.addEventListener('DOMContentLoaded', function() {
 
@@ -39,11 +40,12 @@ const crearLista = (elementos) => {
 
   const nav = crearLista(elementosNav);
   document.querySelector('.contenedor').appendChild(nav);    
+
        const Deportes =document.getElementById('Deportes');
 
                const clickHandler = () =>{
                 if (!eventoEjecutado) {
-                console.log('hola');
+
                 const ElementosEquipo = [
                     { href: '#',id:'Seleccion', texto: 'Seleccion' },
                     { href: '#',id:'Equipo', texto: 'Equipo' },
@@ -90,22 +92,26 @@ const crearLista = (elementos) => {
                   return Card;
                 });
                 return arr;
+
               }
 
+
               const loadSexondMenu = () => {
+                
                   seleccion.addEventListener('click', () => {
                     console.log('holaa');
-    
-                  const cards =[
-                    { href: '#',id:'p1',imagenURL :'IMG/argentina.jpg',texto: 'Argentina lidera el ranking fifa por su desempeño en los ultimos años siendo campeon del mundial en qatar 2022, campon de america y campeon de la finalizima ante italia teniendo un total de 1858 pts"', tituloError : 'No cargo la imagen' },
-                    { href: '#',id:'p2',imagenURL :'IMG/francia.jpg', texto: 'Francia es la segunda seleccion mejor rankeada en el ranking fifa dado a el subcampeonato mundial 2022, campeon 2018 teniendo 1840.59 pts', tituloError : 'No cargo la imagen' },
-                    { href: '#',id:'p3',imagenURL :'IMG/belgica.webp',texto: 'Belgica se queda con el tercer puesto dado que siempre clasifica a los grandes torneos teniendo un invicto de cinco años en su estadio 8 victorias y cinco empates teniendo asi 1795.23 pts en ranking fifa', tituloError : 'No cargo la imagen' },
-                  ];
+                        if (!eventocard){
+                                    const cards =[
+                                      { href: '#',id:'p1',imagenURL :'IMG/argentina.jpg',texto: 'Argentina lidera el ranking fifa por su desempeño en los ultimos años siendo campeon del mundial en qatar 2022, campon de america y campeon de la finalizima ante italia teniendo un total de 1858 pts"', tituloError : 'No cargo la imagen' },
+                                      { href: '#',id:'p2',imagenURL :'IMG/francia.jpg', texto: 'Francia es la segunda seleccion mejor rankeada en el ranking fifa dado a el subcampeonato mundial 2022, campeon 2018 teniendo 1840.59 pts', tituloError : 'No cargo la imagen' },
+                                      { href: '#',id:'p3',imagenURL :'IMG/belgica.webp',texto: 'Belgica se queda con el tercer puesto dado que siempre clasifica a los grandes torneos teniendo un invicto de cinco años en su estadio 8 victorias y cinco empates teniendo asi 1795.23 pts en ranking fifa', tituloError : 'No cargo la imagen' },
+                                    ];
+                                  
+                                    const kard  = CrearCard(cards);
+                                    kard.forEach(el => { document.querySelector('.contenedor3').appendChild(el);});
                 
-                  const kard  = CrearCard(cards);
-                  kard.forEach(el => {
-                    document.querySelector('.contenedor3').appendChild(el);
-                  })
+                                    eventocard = true;
+                                  }
     
                   });
               }
